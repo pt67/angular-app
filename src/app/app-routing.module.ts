a415927ffddc5d9170fa5a5737c9d0f1d94b0918
prakash thapa <pt6769759@gmail.com>
2020-11-26 06:58:19 -0500

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Landingpage } from './app.landingpage';
 import { About } from './app.about';
 import { Contact } from './app.contact';
 
 const routes: Routes = [
+{ path: '', component: Landingpage },
 { path: 'about', component: About },
 { path: 'contact', component: Contact }
 ];
---
+++ src/app/app.landingpage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.landingpage.html',
+  styleUrls: ['./app.component.css']
+})
+export class Landingpage {
+landing_title ="Home page";
+  lpage = 'Landing page';
+}
